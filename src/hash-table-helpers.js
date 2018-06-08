cd2d33d37c4b808d659e3a71ead2ab511692d747
cgoez <goez2001@gmail.com>
2018-06-08 09:37:07 -0700

    Add contains to LinkedList

--- src/hash-table-helpers.js
+++ src/hash-table-helpers.js
@@ -76,6 +76,16 @@ class LinkedList {
 
     return toBeRemoved.value;
   }
+
+  contains(value) {
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (currentNode.value === value) return true;
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
 }
 
 module.exports = {
